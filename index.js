de3d47ed257ab1e28105916c5d1186c9c05a8e31
hejianglin <hejianglin@uino.com>
2023-03-22 17:48:10 +0800

    feat: 修改http协议错误问题

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const Koa = require('koa');
-const request = require('request');
+const request = require('postman-request');
 const bodyParser = require('koa-bodyparser');
 const app = new Koa();
 
@@ -11,12 +11,12 @@ const proxyRequest = (url, method, data) => {
     url,
     method
   }
-  if(data) {
+  if (data) {
     option.formData = data
   }
   return new Promise((resolve, reject) => {
     request(option, (error, response) => {
-      if(error) reject(error)
+      if (error) reject(error)
       let _data = null;
       try {
         _data = JSON.parse(response.body);
@@ -28,22 +28,31 @@ const proxyRequest = (url, method, data) => {
   })
 }
 
+const modifyUrl = (url) => {
+  const isCorret = url.indexOf('//') !== -1;
+  if (isCorret) return url;
+  return url.split("/").map((i, index) => {
+    if (index === 0) {
+      return i + '/'
+    }
+    return i
+  }).join("/")
+}
 app.use(bodyParser())
 // 加载路由中间件
 app.use(async (ctx) => {
-  const url = ctx.originalUrl.slice(1)
+  let url = ctx.originalUrl.slice(1)
   const method = ctx.method
   const bodyData = ctx.request.body
-  // console.log(ctx.query, 'query')
-  // console.log(ctx.request.body, "request body")
-  console.log(url)
-  if(!url) {
-    ctx.body = "please provide a url"
+  const isUrl = url.startsWith('http:') || url.startsWith('https')
+  if (!url || !isUrl) {
+    ctx.body = "please provide a correct url to request"
   } else {
+    url = modifyUrl(url)
     ctx.body = await proxyRequest(url, method, bodyData)
   }
 })
 
 app.listen(3008, () => {
-    console.log('3008项目启动')
+  console.log('3008项目启动')
 });
